5cf63b302de822d77bab9d3be37f27c8e2fb6554
Andriy Massimilla <andriymassimilla@gmail.com>
2017-10-31 08:11:20 -0400

    Add some basic stuff

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -3,5 +3,13 @@ import java.awt.geom.Point2D;
 public class Algorithm {
     public static Point2D[] boundaryPoints;
 
+    public static Point2D[] makeRect(Point2D p1, Point2D p2) {
+        Point2D[] points = new Point2D[4];
+        points[0] = new Point2D.Double(p1.getX(), p1.getY());
+        points[1] = new Point2D.Double(p2.getX(), p1.getY());
+        points[2] = new Point2D.Double(p2.getX(), p2.getY());
+        points[3] = new Point2D.Double(p1.getX(), p2.getY());
 
+        return points;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,27 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.awt.geom.Point2D;
+
 public class Main extends Application {
     public static void main(String[] args) {
+        Algorithm.boundaryPoints = new Point2D[7];
+        Algorithm.boundaryPoints[0] = new Point2D.Double(0, 0);
+        Algorithm.boundaryPoints[1] = new Point2D.Double(200, 0);
+        Algorithm.boundaryPoints[2] = new Point2D.Double(200, 200);
+        Algorithm.boundaryPoints[3] = new Point2D.Double(400, 200);
+        Algorithm.boundaryPoints[4] = new Point2D.Double(400, 400);
+        Algorithm.boundaryPoints[5] = new Point2D.Double(0, 400);
+        Algorithm.boundaryPoints[6] = new Point2D.Double(0, 0);
+
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Drawing Operations Test");
+        primaryStage.setTitle("Algorithm Simulation");
         Group root = new Group();
-        Canvas canvas = new Canvas(300, 250);
+        Canvas canvas = new Canvas(800, 600);
         GraphicsContext gc = canvas.getGraphicsContext2D();
         drawShapes(gc);
         root.getChildren().add(canvas);
@@ -24,9 +35,12 @@ public class Main extends Application {
     }
 
     private void drawShapes(GraphicsContext gc) {
-        gc.setFill(Color.GREEN);
+        gc.setFill(Color.BLACK);
         gc.setStroke(Color.BLUE);
-        gc.setLineWidth(5);
-        gc.fillOval(10, 60, 10, 10);
+        gc.setLineWidth(3);
+
+        for (int i = 0; i < Algorithm.boundaryPoints.length; i++) {
+            gc.strokeLine(Algorithm.boundaryPoints[i].getX(), Algorithm.boundaryPoints[i].getY(), Algorithm.boundaryPoints[i+1].getX(), Algorithm.boundaryPoints[i+1].getY());
+        }
     }
 }
