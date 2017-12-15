a0ff4b6c4851de1bf2a8b71f3cc8c877b8d0cf6d
Daniel Chen <danieljnchen@gmail.com>
2017-12-15 08:55:23 -0500

    Raycast integration

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -89,7 +89,7 @@ public class Algorithm {
 
         while (lengthCovered < getMaxLength(0)) {
             Raycast ray1 = new Raycast(curPoint, angle);
-            Raycast ray2 = new Raycast(ray1.hitPoint, angle + 180);
+            Raycast ray2 = new Raycast(ray1.getHitPoint(), angle + 180);
 
             curPoint = new Point2D(curPoint.getX(), curPoint.getY() + robot.length);
             lengthCovered += robot.length;
@@ -109,17 +109,17 @@ public class Algorithm {
 
         // Raycast to determine distance
         Raycast cast = new Raycast(startPoint, angle);
-        if (cast.hitPoint == null) return -1;
+        if (cast.getHitPoint() == null) return -1;
 
         Point2D currentPoint = new Point2D(startPoint.getX(), startPoint.getY());
 
-        while (currentPoint.distance(cast.hitPoint) > robot.length) {
+        while (currentPoint.distance(cast.getHitPoint()) > robot.length) {
             // Raycast to the left and right to find our max length
             Raycast left = new Raycast(currentPoint, angle - Math.PI/2);
             Raycast right = new Raycast(currentPoint, angle + Math.PI/2);
 
-            if (left.hitPoint.distance(right.hitPoint) > maxLength) {
-                maxLength = left.hitPoint.distance(right.hitPoint);
+            if (left.getHitPoint().distance(right.getHitPoint()) > maxLength) {
+                maxLength = left.getHitPoint().distance(right.getHitPoint());
             }
 
             currentPoint = new Point2D(currentPoint.getX() + Math.cos(angle), currentPoint.getY() + Math.sin(angle));
