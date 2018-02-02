971193ce7b6842689f133000ccd71657af9287e5
Andriy Massimilla <andriymassimilla@gmail.com>
2018-02-02 08:18:45 -0500

    Fix robot dimensions

--- src/Robot.java
+++ src/Robot.java
@@ -1,6 +1,7 @@
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+
 import java.util.ArrayList;
 
 class Robot extends UIObject {
@@ -38,20 +39,20 @@ class Robot extends UIObject {
     public void draw(GraphicsContext gc) {
         // Robot shape
         gc.setFill(Color.ORANGE);
-        gc.fillRect(pos.getX() - length /2, pos.getY() - width /2, length, width);
+        gc.fillRect(pos.getX() - width / 2, pos.getY() - length / 2, width, length);
 
         // Planned path
         gc.setLineWidth(5);
         gc.setStroke(Color.RED);
         for (int i = 0; i < pathNodes.size() - 1; i++) {
-            gc.strokeLine(pathNodes.get(i).getX(), pathNodes.get(i).getY(), pathNodes.get(i+1).getX(), pathNodes.get(i+1).getY());
+            gc.strokeLine(pathNodes.get(i).getX(), pathNodes.get(i).getY(), pathNodes.get(i + 1).getX(), pathNodes.get(i + 1).getY());
         }
 
         if (pathNodes.size() != 0) {
-            if(pos == Point2D.ZERO) {
+            if (pos == Point2D.ZERO) {
                 pos = pathNodes.get(0);
             }
-            gc.fillOval(pathNodes.get(pathNodes.size()-1).getX(), pathNodes.get(pathNodes.size()-1).getY(), 5, 5);
+            gc.fillOval(pathNodes.get(pathNodes.size() - 1).getX(), pathNodes.get(pathNodes.size() - 1).getY(), 5, 5);
             approachNextNode();
         }
     }
